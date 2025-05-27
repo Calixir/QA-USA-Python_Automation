dce715523e870df37b28600ab86c1f581b2ffe87
Calixir <joshuaoliveras23@gmail.com>
2025-05-26 21:17:11 -0400

    Initial Comment

--- data.py
+++ data.py
@@ -1,1 +1,9 @@
 # File with constants
+URBAN_ROUTES_URL = 'https://cnt-6691e3be-d028-4711-bda9-4c4eb30d1b81.containerhub.tripleten-services.com'
+
+ADDRESS_FROM = 'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE = '1111'
+MESSAGE_FOR_DRIVER = 'Stop at the juice bar, please'
--- main.py
+++ main.py
@@ -1,2 +1,53 @@
+import data
+import helpers
 
 class TestUrbanRoutes:
+    @classmethod
+    def setup_class(cls):
+        helpers.is_url_reachable(data.URBAN_ROUTES_URL)
+        if helpers.is_url_reachable(data.URBAN_ROUTES_URL) == True:
+            print('Connected to Urban Routes Server')
+        else:
+            print('Cannot connect to Urban Routes. Check the server is on and still running')
+    def test_set_route(self):
+        # Add in S8
+        print("test_set_route")
+        pass
+
+    def test_select_plan(self):
+        # Add in S8
+        print("test_select_plan")
+        pass
+
+    def test_fill_phone_number(self):
+        # Add in S8
+        print("test_fill_phone_number")
+        pass
+
+    def test_fill_card(self):
+        # Add in S8
+        print("test_fill_card")
+        pass
+
+    def test_comment_for_driver(self):
+        # Add in S8
+        print("test_comment_for_driver")
+        pass
+
+    def test_order_blanket_and_handkerchiefs(self):
+        # Add in S8
+        print("test_order_blanket_and_handkerchiefs")
+        pass
+
+    def test_order_2_ice_creams(self):
+        for i in range(2):
+            # Add in S8
+            print("Icecream x{i}")
+            pass
+
+
+    def test_car_search_model_appears(self):
+        # Add in S8
+        print("test_car_search_model_appears")
+        pass
+
